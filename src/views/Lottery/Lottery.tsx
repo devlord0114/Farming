cc77e5d19d80689ad1cfeca00090cc8eef041d30
SuperWebMan <aleksandrshreider@outlook.com>
2021-10-16 16:52:07 +0900

    Delete history provider

--- src/views/Lottery/Lottery.tsx
+++ src/views/Lottery/Lottery.tsx
@@ -86,19 +86,8 @@ const Lottery: React.FC = () => {
   const { account } = useWallet()
   const TranslateString = useI18n()
   const [activeIndex, setActiveIndex] = useState(0)
-  const [historyData, setHistoryData] = useState([])
-  const [historyError, setHistoryError] = useState(false)
   const [currentLotteryNumber, setCurrentLotteryNumber] = useState(0)
   const [mostRecentLotteryNumber, setMostRecentLotteryNumber] = useState(1)
-
-  useEffect(() => {
-    fetch(`https://api.pancakeswap.com/api/lotteryHistory`)
-      .then((response) => response.json())
-      .then((data) => setHistoryData(data))
-      .catch(() => {
-        setHistoryError(true)
-      })
-  }, [])
 
   useEffect(() => {
     const getInitialLotteryIndex = async () => {
@@ -122,20 +111,16 @@ const Lottery: React.FC = () => {
     <>
       <Page style={{ marginTop: '0px', backgroundColor: 'white', borderRadius: '24px' }}>
         <Title>{TranslateString(290, 'Lottery')}</Title>
-        <PastLotteryDataContext.Provider
-          value={{ historyError, historyData, mostRecentLotteryNumber, currentLotteryNumber }}
-        >
-          <Wrapper>
-            <Content>
-              <NextDrawPage />
-              <PastDrawsPage />
-            </Content>
-            <Prize>
-              <YourPrizesCard />
-              <UnlockWalletCard />
-            </Prize>
-          </Wrapper>
-        </PastLotteryDataContext.Provider>
+        <Wrapper>
+          <Content>
+            <NextDrawPage />
+            <PastDrawsPage />
+          </Content>
+          <Prize>
+            <YourPrizesCard />
+            <UnlockWalletCard />
+          </Prize>
+        </Wrapper>
         <HowItWorks />
       </Page>
     </>
